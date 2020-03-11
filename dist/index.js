71f701a740802ee70fcd33836f994032817b4a0e
Simon Solders <simon.solders@gmail.com>
2020-03-11 10:52:20 +0100

    Compile Field-constructor fix

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,29 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var _api=require("./sdk/api");Object.defineProperty(exports,"HostedFields",{enumerable:!0,get:function(){return _api.HostedFields}});var _index=require("./sdk/model/field/index.js");Object.defineProperty(exports,"Field",{enumerable:!0,get:function(){return _index.Field}}),Object.defineProperty(exports,"FieldTypes",{enumerable:!0,get:function(){return _index.FieldTypes}});
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _api = require("./sdk/api");
+
+Object.defineProperty(exports, "HostedFields", {
+  enumerable: true,
+  get: function get() {
+    return _api.HostedFields;
+  }
+});
+
+var _index = require("./sdk/model/field/index.js");
+
+Object.defineProperty(exports, "Field", {
+  enumerable: true,
+  get: function get() {
+    return _index.Field;
+  }
+});
+Object.defineProperty(exports, "FieldTypes", {
+  enumerable: true,
+  get: function get() {
+    return _index.FieldTypes;
+  }
+});
